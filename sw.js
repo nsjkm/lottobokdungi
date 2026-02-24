a68d3f9240811d2b13d9c78f47aba33069d35bb3
nsjkm <shtjdwhd1@gmail.com>
2026-02-25 01:01:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'lottobokdungi-v1';
-const urlsToCache = ['/', '/index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+// 서비스워커 - 캐시 완전 비활성화
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // 캐시 전혀 사용 안 함 - 항상 네트워크에서 직접 가져옴
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+  );
 });

f79b0a62cacbfeb2e521813c02c2157a7deead10
nsjkm <shtjdwhd1@gmail.com>
2026-02-25 01:12:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-// 서비스워커 - 캐시 완전 비활성화
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
+// 캐시 완전 초기화
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  // 캐시 전혀 사용 안 함 - 항상 네트워크에서 직접 가져옴
-  event.respondWith(
-    fetch(event.request, { cache: 'no-store' })
-  );
+  event.respondWith(fetch(event.request, {cache: 'no-store'}));
 });
